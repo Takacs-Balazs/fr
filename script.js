253d7e765eb23ba776bc94061147257f1fe4f146
Takacs-Balazs <71711153+Takacs-Balazs@users.noreply.github.com>
2024-04-26 09:06:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,400 @@
+const kerdesek = [
+    {
+        temakor: "Ki mondta? - Az ember tragédiája",
+        kerdes: "Be van fejezve a nagy mű, igen. A gép forog, az alkotó pihen.",
+        valasz: "Az Úr",
+        kepek: {
+            kep1: "pihen.jpg"
+        }
+    },
+    {
+        temakor: "Ki mondta? - Az ember tragédiája",
+        kerdes: "Van-é jutalma a nemes kebelnek?",
+        valasz: "Ádám",
+        kepek: {}
+    },
+    {
+        temakor: "Ki mondta? - Az ember tragédiája",
+        kerdes: "Mondottam, ember: küzdj és bízva bízzál!",
+        valasz: "Az Úr",
+        kepek: {}
+    },
+    {
+        temakor: "Ki mondta? - Az ember tragédiája",
+        kerdes: "Rettentő képek, óh, hová levétek?",
+        valasz: "Ádám",
+        kepek: {
+            kep1: "nightmare.jpg"
+        }
+    },
+    {
+        temakor: "Ki mondta? - Az ember tragédiája",
+        kerdes: "Uram, legyőztél. Ím, porban vagyok Nélkűled, ellened hiába vívok:",
+        valasz: "Ádám",
+        kepek: {}
+    },
+    {
+        temakor: "Melyik színben történik? - Az ember tragédiája",
+        kerdes: "Ádám kerítést épít",
+        valasz: "3. szín",
+        kepek: {
+            kep1: "fence.png"
+        }
+    },
+    {
+        temakor: "Melyik színben történik? - Az ember tragédiája",
+        kerdes: "Éva egy rabszolga felesége lesz",
+        valasz: "4. szín",
+        kepek: {
+            kep1: "rab.jpg"
+        }
+    },
+    {
+        temakor: "Melyik színben történik? - Az ember tragédiája",
+        kerdes: "Ádám elalszik",
+        valasz: "8. szín",
+        kepek: {
+            kep1: "sleep.jpg"
+        }
+    },
+    {
+        temakor: "Melyik színben történik? - Az ember tragédiája",
+        kerdes: "Ádám és Lucifer felülről néznek le Londonra",
+        valasz: "11. szín",
+        kepek: {}
+    },
+    {
+        temakor: "Melyik színben történik? - Az ember tragédiája",
+        kerdes: "Ádám rájön hogy az embernek legalább az akarata szabad",
+        valasz: "15. szín",
+        kepek: {}
+    },
+    {
+        temakor: "Helyszínek - Az ember tragédiája",
+        kerdes: "Egyiptom",
+        valasz: "3. szín",
+        kepek: {
+            kep1: "egyipt.jpg"
+        }
+    },
+    {
+        temakor: "Helyszínek - Az ember tragédiája",
+        kerdes: "Róma",
+        valasz: "6. szín",
+        kepek: {}
+    },
+    {
+        temakor: "Helyszínek - Az ember tragédiája",
+        kerdes: "Konstantinápoly",
+        valasz: "7. szín",
+        kepek: {}
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Woland Professzor",
+        valasz: "Sátán",
+        kepek: {
+            kep1: "satan.jpg",
+            kep2: "professor_woland.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Annuska",
+        valasz: "Kiönti az olajat, ami Berlioz halálához vezet",
+        kepek: {
+            kep1: "annuska.jfif",
+            kep2: "oil_spilling.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Ivan Nyikolajevics",
+        valasz: "Művészneve Hontalan",
+        kepek: {
+            kep1: "elmegyogyintezet.jfif",
+            kep2: "no-christ.jfif",
+            kep3: "writer.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Margarita Nyikolajevna",
+        valasz: "A Mester szerelme",
+        kepek: {
+            kep1: "flying-girl.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Mester",
+        valasz: "Regényt ír Ponczius Pilátustól",
+        kepek: {
+            kep1: "regeny.jfif",
+            kep2: "writer.jfif",
+            kep3: "elmegyogyintezet.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Azazelló",
+        valasz: "Woland szolgálója, krémet ad Margaritának, amitől repülni tud",
+        kepek: {
+            kep1: "azazello.jfif",
+            kep2: "azazello2.jfif",
+            kep3: "azazello3.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Behemót",
+        valasz: "Woland szolgálója, Kandúr alakjában két lábon jár",
+        kepek: {
+            kep1: "behemot.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Hella",
+        valasz: "Woland szolgálója, vörös hajú vámpír",
+        kepek: {
+            kep1: "hella.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Korovjov",
+        valasz: "Woland szolgálója, pepita zakós nyakigláb, valódi formája: soha nem mosolygó sötét lovag",
+        kepek: {}
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Berlioz",
+        valasz: "Főszerkesztő, elcsúszik az olajon és a villamos levágja a fejét",
+        kepek: {
+            kep1: "elcsuszas.jfif",
+            kep2: "villamos.jfif",
+            kep3: "guillotine.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Ponczius Pilátus",
+        valasz: "Júdea 5. helytartója",
+        kepek: {
+            kep1: "ponczius-pilatus.jfif"
+        }
+    },
+    {
+        temakor: "A Mester és Margarita szereplői",
+        kerdes: "Jesua Ha-Nocri",
+        valasz: "Jézus",
+        kepek: {
+            kep1: "jezus.jfif",
+            kep2: "cross.jfif"
+        }
+    },
+    {
+        temakor: "Herczeg Ferenc: Az élet kapuja szereplők",
+        kerdes: "Bakócz Tamás",
+        valasz: "Célja Szent Péter kulcsának megszerzése (pápaság)",
+        kepek: {
+            kep1: "bakócz-tamás.jfif"
+        }
+    },
+    {
+        temakor: "Herczeg Ferenc: Az élet kapuja szereplők",
+        kerdes: "Vértesi Tamás",
+        valasz: "Szerelmes Fiamettába",
+        kepek: {
+            kep1: "vertesi-tamas.jpg"
+        }
+    },
+    {
+        temakor: "Herczeg Ferenc: Az élet kapuja szereplők",
+        kerdes: "Fiametta",
+        valasz: "Chiginek adja a titkos listát",
+        kepek: {
+            kep1: "fiametta.jpg"
+        }
+    },
+    {
+        temakor: "Herczeg Ferenc: Az élet kapuja szereplők",
+        kerdes: "II. Julius",
+        valasz: "Jelenlegi pápa, célja a Szent Péter bazilika befejezése, Róma fényének őrzése",
+        kepek: {
+            kep1: "II-Julius.jfif"
+        }
+    },
+    {
+        temakor: "Herczeg Ferenc: Az élet kapuja szereplők",
+        kerdes: "Medici",
+        valasz: "Ő lesz az új pápa (X. Leó pápa)",
+        kepek: {
+            kep1: "medici-cimer.png",
+            kep2: "szent-peter-bazilika.jfif",
+            kep3: "medici.jpg"
+        }
+    }
+];
+
+let kvízKonténer;
+let helyesValaszokSzama = 0;
+let kerdesIndexek = [];
+let valaszok = [];
+let jelenlegiKérdésMegválaszolva = false;
+let tema;
+TemaKivalasztasa();
+
+let randomIndex;
+let randomSzam;
+
+function TemaKivalasztasa() {
+    kvízKonténer = null;
+    helyesValaszokSzama = 0;
+    kerdesIndexek = [];
+    valaszok = [];
+    jelenlegiKérdésMegválaszolva = false;
+    tema = null;
+
+    const temakorok = document.createElement("select");
+    temakorok.name = "temakorok";
+    temakorok.id = "temakorok";
+    document.body.appendChild(temakorok);
+
+    const ujButton = document.createElement("button");
+    ujButton.id = "inditoGomb";
+    ujButton.innerText = "Kiválaszt";
+    ujButton.addEventListener("click", Inditas);
+    document.body.appendChild(ujButton);
+
+    //Témakörök felsorolása
+    let options = new Set();
+    for (const tema of kerdesek) {
+        if (!options.has(tema.temakor)) {
+            options.add(tema.temakor);
+        }
+    }
+    for (let index = 0; index < options.size; index++) {
+        const ujOption = document.createElement("option");
+        ujOption.innerText = Array.from(options)[index];
+        temakorok.appendChild(ujOption);
+    }
+}
+
+function Inditas() {
+    //Téma kiválasztása
+    tema = document.getElementById("temakorok").value;
+    for (let index = 0; index < kerdesek.length; index++) {
+        const kerdes = kerdesek[index];
+        if (kerdes.temakor == tema) {
+            kerdesIndexek.push(index);
+            valaszok.push(kerdes.valasz);
+        }
+    }
+
+    //Témakörök eltávolítása
+    const temakorok = document.getElementById("temakorok");
+    const inditoGomb = document.getElementById("inditoGomb");
+    document.body.removeChild(temakorok);
+    document.body.removeChild(inditoGomb)
+
+    //Kvíz felrakása
+    const ujKvizcontainer = document.createElement("div");
+    ujKvizcontainer.id = "kvíz-konténer";
+    ujKvizcontainer.classList.add("konténer");
+    document.body.appendChild(ujKvizcontainer);
+    kvízKonténer = document.getElementById('kvíz-konténer');
+    kvízKonténer.innerHTML = "";
+
+    //Indítás
+    randomIndex = Math.floor(Math.random() * kerdesIndexek.length);
+    randomSzam = kerdesIndexek[randomIndex];
+    addKártya(randomSzam);
+    kerdesIndexek.splice(randomIndex, 1);
+}
+
+function következőKérdés() {
+    if (kerdesIndexek.length != 0) {
+        randomIndex = Math.floor(Math.random() * kerdesIndexek.length);
+        randomSzam = kerdesIndexek[randomIndex];
+        if (!jelenlegiKérdésMegválaszolva) return;
+        jelenlegiKérdésMegválaszolva = false;
+        setTimeout(() => {
+            addKártya(randomSzam);
+            kerdesIndexek.splice(randomIndex, 1);
+        }, 1500);
+    }
+    else {
+        window.alert(`Befejezted a kvízt! Helyes válaszok: ${helyesValaszokSzama}/${valaszok.length}=${(helyesValaszokSzama / valaszok.length * 100).toFixed(2)}%`);
+        window.location.href = "index.html";
+    }
+}
+
+function addKártya(index) {
+    //Kártya
+    const kártya = document.createElement('div');
+    kártya.classList.add('kártya');
+    const elem = kerdesek[index];
+    //Kérdés
+    const kerdes = document.createElement("h3");
+    kerdes.innerText = elem.kerdes;
+    kártya.appendChild(kerdes);
+    //Képek
+    const kepDiv = document.createElement("div");
+    kepDiv.classList.add("kepDiv");
+    for (const kep in elem.kepek) {
+        const img = document.createElement("img");
+        img.classList.add("kep");
+        img.src = `Images/${elem.kepek[kep]}`;
+        kepDiv.appendChild(img);
+    }
+    kártya.appendChild(kepDiv);
+    //Válaszok
+    const valaszokDiv = document.createElement("div");
+    valaszokDiv.classList.add("lehetőségek");
+    //Válasz
+    let helytelenValaszok = valaszok.filter(valasz => valasz !== elem.valasz);
+    let helyesValaszPozicio = Math.floor(Math.random() * 4);
+    let randomValaszIndex = Math.floor(Math.random() * helytelenValaszok.length);
+    for (let index = 0; index < 4; index++) {
+        const valaszButton = document.createElement("button");
+        valaszButton.classList.add("lehetőség");
+        if (index == helyesValaszPozicio) {
+            valaszButton.innerText = elem.valasz;
+        } else {
+            valaszButton.innerText = helytelenValaszok[randomValaszIndex];
+        }
+
+        valaszButton.addEventListener('click', () => {
+            if (valaszButton.textContent == elem.valasz) {
+                valaszButton.style.backgroundColor = 'lightgreen';
+                jelenlegiKérdésMegválaszolva = true;
+                helyesValaszokSzama++;
+                következőKérdés();
+            }
+            else {
+                valaszButton.style.backgroundColor = 'red';
+                jelenlegiKérdésMegválaszolva = true;
+                const lehetőségek = kártya.querySelectorAll('.lehetőség');
+                lehetőségek.forEach(element => {
+                    if (element.textContent == elem.valasz) {
+                        element.style.backgroundColor = 'lightgreen';
+                    }
+                });
+                következőKérdés();
+            }
+            let gombok = document.querySelectorAll(".lehetőség");
+            gombok.forEach(element => {
+                element.disabled = true;
+            });
+        });
+
+        valaszokDiv.appendChild(valaszButton);
+        helytelenValaszok = helytelenValaszok.filter(valasz => valasz !== helytelenValaszok[randomValaszIndex])
+        randomValaszIndex = Math.floor(Math.random() * helytelenValaszok.length);
+    }
+    //Kártya hozzáadása
+    kártya.appendChild(valaszokDiv);
+    kvízKonténer.appendChild(kártya);
+}
